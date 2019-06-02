3178021b15b93c86c8e946687602ed78a6e3303f
Rebecca Mrowiec <49169512+rebeccamrowiec@users.noreply.github.com>
2019-06-02 18:32:48 +0100

    Continue using state and if statements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -10,23 +11,32 @@ import ReactDOM from 'react-dom';
 // };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { lat: null };
+  //   this.state = { lat: null, errorMessage: '' };
+  // }
 
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // We called setstate
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })  
     );
   }
   
   // In react we have to define render
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.setState.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
