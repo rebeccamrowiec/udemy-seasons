9c9622869f8b8b5c39c8a2bcb6f6fceff68e9dc8
Rebecca Mrowiec <49169512+rebeccamrowiec@users.noreply.github.com>
2019-06-03 11:25:17 +0100

    Add styling and a loading animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -36,7 +37,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
